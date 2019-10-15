50d403cf7b2cb6af9fd281cb4c136939b84a6b61
mattchley <mattchley@gmail.com>
2019-10-15 11:56:31 -0700

    pseudocode

---
+++ Jquery.js
@@ -0,0 +1,21 @@
+// Jquery code for Daily planner
+var timeArrayEl = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+
+
+$(document).ready(function () {
+// Time running function
+
+// Current Day Generator
+
+
+// Time block variables
+
+// display date at top
+
+// color blocks based on past present and future
+
+// saving function
+
+// event listener for save content
+
+// generate saved content function
